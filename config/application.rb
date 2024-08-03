fd2e10f960d89e5209a5d75afa1ca4ea341435bb
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-08-03 15:56:42 +0500

    Setup and configure testing

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,18 @@ module Socialite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_specs: false,
+                       system_specs: true,
+                       model_specs: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
