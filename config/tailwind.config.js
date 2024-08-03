fa239937c38968badcec466c022cc6c406e2ec53
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-08-03 16:46:26 +0500

    Add Post scaffold and remove MiniTest files

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {
@@ -18,5 +19,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
+    require('flowbite/plugin')
   ]
 }
