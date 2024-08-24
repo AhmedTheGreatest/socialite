90630493da55f8f70719f7fee975d4e823182892
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-08-24 13:35:08 +0500

    Added Model Validations to Post and Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :profile
   belongs_to :post
+
+  validates :profile, :post, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :profile
   has_many :likes, dependent: :destroy
   has_many :likers, through: :likes, source: :profile
+
+  validates :profile, presence: true
+  validates :body, presence: true, length: {minimum: 2, maximum: 2000}
 end
