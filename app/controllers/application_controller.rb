b3d2fcab977351ea3543429e164a1bd49d078871
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-08-24 17:20:28 +0500

    Fix bug with User Registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :check_profile!
+  before_action :check_profile!, unless: :devise_controller?
 
   private
 
   def check_profile!
-    if !current_user.profile
+    if current_user && !current_user.profile
       redirect_to new_profile_path
     end
   end
