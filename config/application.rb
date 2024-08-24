08ac694f75b7a576afc9c12d74a40ce649f3f3b8
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-08-24 13:28:54 +0500

    Modify Rails Generators and Remove unnecessary tests

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,13 @@ module Socialite
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.generators do |g|
-      g.test_framework :rspec
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false,
+        controller_specs: false
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
